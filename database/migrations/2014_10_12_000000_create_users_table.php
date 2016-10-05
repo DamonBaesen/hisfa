3c9afe0c0bfabe9e0a051a949e035e3ca7ce3047
wimvermost <vermost.w@gmail.com>
2016-10-05 14:40:31 +0200

    Added foto to users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-			$table->string('email');
+			$table->string('foto');
             $table->rememberToken();
             $table->timestamps();
         });
