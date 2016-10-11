18c02041d484860237dc4785fbf74d189f9f37fe
IGenerate <brent.stappaerts@gmail.com>
2016-10-11 11:50:16 +0200

    Account view -> part 1

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -1,10 +1,23 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>HISFA -- Account</title>
-</head>
-<body>
-        HIER KOMT NOG IETS
-</body>
-</html>
+@extends('layouts.master')
+
+@section('content')
+<div class="container">
+    <div class="row">
+            <div class="panel panel-default" id="form">
+                <h1>HISFA</h1>
+                <h3>My account</h3>
+
+                  <div class="row" id="myaccount">
+                     <div class="col-xs-6">
+                        <img src="http://www.inventoree.net/images/Dan_Raymond.png" />
+                      </div>
+                      <div class="col-xs-6">
+                        <h5><strong>Name: Tom </strong></h5>
+                        <h5><strong>Email: tom@exemple.com </strong></h5>
+                        <h5><strong>Password: ****** </strong></h5>
+                      </div>
+                  </div>
+              </div>
+    </div>
+</div>
+@endsection
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -12,6 +12,7 @@
 
     <!-- Styles -->
     <link href="/css/app.css" rel="stylesheet">
+    <link href="css/forms.css" rel="stylesheet">
 
 </head>
 <body>
