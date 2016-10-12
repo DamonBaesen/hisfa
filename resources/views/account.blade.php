ff3891999efa55499572273afb473075c6f89a07
DamonBaesen <r0484656@student.thomasmore.be>
2016-10-12 11:36:37 +0200

    fix merge Brent

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -1,23 +1,52 @@
-@extends('layouts.master')
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
 
-@section('content')
+    <title>Account</title>
+</head>
+<body>
+
 <div class="container">
     <div class="row">
-            <div class="panel panel-default" id="form">
-                <h1>HISFA</h1>
-                <h3>My account</h3>
-
-                  <div class="row" id="myaccount">
-                     <div class="col-xs-6">
-                        <img src="http://www.inventoree.net/images/Dan_Raymond.png" />
-                      </div>
-                      <div class="col-xs-6">
-                        <h5><strong>Name: Tom </strong></h5>
-                        <h5><strong>Email: tom@exemple.com </strong></h5>
-                        <h5><strong>Password: ****** </strong></h5>
-                      </div>
-                  </div>
-              </div>
+        <div class="col-md-10 col-md-offset-1">
+            <img src="/uploads/avatars/{{ Auth::user()->foto }}" style="width:150px; height:150px; float:left; border-radius:50%; margin-right:25px;">
+            <h2> {{ Auth::user()->name }}'s profiel</h2>
+            <form enctype="multipart/form-data" action="/account/updatephoto" method="POST">
+                <label>Update Profile Image</label>
+                <input type="file" name="avatar">
+                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                <input type="submit" class="pull-right btn btn-sm btn-primary">
+            </form>
+        </div>
     </div>
 </div>
-@endsection
+
+@if(session('message'))
+    {{ session('message') }}<br>
+@endif
+
+<h1>Accountgegevens</h1>
+<form action="/account/changeuserinformation" method="post">
+    <input name="_token" type="hidden" value="{!! csrf_token() !!}" />
+    <input type="text" value=" {{ Auth::user()->name }}" name="name">
+    <input type="text" value=" {{ Auth::user()->email }}" name="email">
+    <label for="checkbox_mails">Ik wil mails ontvangen</label>
+    @if(Auth::user()->name)
+        <input type="checkbox" name="checkbox_mail" value="1" checked>
+    @else
+        <input type="checkbox" name="checkbox_mail" value="0" >
+    @endif
+    <input type="submit" value="Gegevens aanpassen">
+</form>
+
+<h1>Wachtwoord veranderen</h1>
+<form action="/account/changepassword" method="post">
+    <input name="_token" type="hidden" value="{!! csrf_token() !!}" />
+    Wachtwoord: <input type="password" name="input_password1"><br>
+    Wachtwoord bevestigen: <input type="password" name="input_password2"><br>
+    <input type="submit" value="Wachtwoord aanpassen">
+</form>
+
+</body>
+</html>
