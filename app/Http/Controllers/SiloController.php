b330d8c3d8496d62c77282dfa7c7adf6ba3b4c01
DamonBaesen <r0484656@student.thomasmore.be>
2016-10-16 21:14:59 +0200

    routes + form silo edit

--- app/Http/Controllers/SiloController.php
+++ app/Http/Controllers/SiloController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Primesilo;
+use App\Rawmaterial;
 use DB;
 use App\Quotation;
 use Illuminate\Support\Facades\Input;
@@ -34,7 +35,7 @@ class SiloController extends Controller
         $id = Input::get('textName');
 
         DB::table('primesilos')->insert(
-            array('quantity' => '0', 'id' => $id)
+            array('quantity' => '0', 'id' => $id, 'rawmaterial_id' => 6)
         );
 
         $primesiloinhoud = \App\Primesilo::all();
@@ -66,6 +67,8 @@ class SiloController extends Controller
         $primesiloinhoud = \App\Primesilo::all();
         $data['primesilo'] = $primesiloinhoud;
 
+
+
         if($quantity >= 90){
             app('App\Http\Controllers\EmailController')->send($id, $quantity);
         }
@@ -80,6 +83,10 @@ class SiloController extends Controller
         $data['primesilos'] = $primesiloinhouds;
 
         $data['primesilo'] = $primesiloinhoud;
+
+        $rawmaterial = \App\Rawmaterial::all();
+        $data['rawmaterial'] = $rawmaterial;
+
         return view('siloedit', $data);
     }
 
