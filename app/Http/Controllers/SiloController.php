539f3f6242aeb706230d04f4ba04c3317b393b6a
DamonBaesen <r0484656@student.thomasmore.be>
2016-10-29 11:57:14 +0200

    change paths in view + nug fixes

--- app/Http/Controllers/SiloController.php
+++ app/Http/Controllers/SiloController.php
@@ -31,7 +31,7 @@ class SiloController extends Controller
         $rawmaterial = \App\Rawmaterial::all();
         $data['rawmaterial'] = $rawmaterial;
 
-        return view('silo', $data);
+        return view('/silo/silo', $data);
     }
 
     public function add()
@@ -42,23 +42,19 @@ class SiloController extends Controller
             array('quantity' => '0', 'id' => $id, 'rawmaterial_id' => 6)
         );
 
-        $primesiloinhoud = \App\Primesilo::all();
-        $data['primesilo'] = $primesiloinhoud;
-        return view('silo', $data);
+        return redirect('/silo/');
     }
 
     public function addShow()
     {
-        return view('siloadd');
+        return view('/silo/siloadd');
     }
 
     public function remove($id)
     {
         DB::table('primesilos')->where('id', '=', $id)->delete();
 
-        $primesiloinhoud = \App\Primesilo::all();
-        $data['primesilo'] = $primesiloinhoud;
-        return view('silo', $data);
+        return redirect('/silo/');
     }
 
     public function edit($id)
@@ -68,15 +64,10 @@ class SiloController extends Controller
         $quantity = Input::get('txtHoeveelheid');
         \App\Primesilo::where('id', '=', $id)->update(array('id' => $newID, 'quantity' => $quantity,'rawmaterial_id' => $rawmaterialID));
 
-        $primesiloinhoud = \App\Primesilo::all();
-        $data['primesilo'] = $primesiloinhoud;
-
-
-
         if($quantity >= 90){
             app('App\Http\Controllers\EmailController')->send($id, $quantity);
         }
-        return view('silo', $data);
+        return redirect('/silo/');
     }
 
     public function editShow($id)
@@ -91,7 +82,7 @@ class SiloController extends Controller
         $rawmaterial = \App\Rawmaterial::all();
         $data['rawmaterial'] = $rawmaterial;
 
-        return view('siloedit', $data);
+        return view('/silo/siloedit', $data);
     }
 
 }
