9de969ce36f33cf45a9a7c01cd3cfc8594f47cd1
Bart Meyvis <Bart.meyvis@hotmail.com>
2016-11-02 14:38:17 +0100

    dashboard responsive patch fix

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,7 +13,7 @@
 
     <div class="dash-container">
         <div class="left">
-            <div class="stock">
+            <!--<div class="stock">
                 <div class="btn-group stock-menu">
                     <button type="button" class="btn dashboard-stock-select-btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> P15 </button>
                     <div class="dropdown-menu">
@@ -35,9 +35,36 @@
 
                 </div>
 
+            </div>-->
+            <div class="frame stock">
+                <div class="frame-title">
+                    <h2>Blocks in storage</h2> </div>
+                <div class="btn-group stock-menu">
+                    <button type="button" class="btn dashboard-stock-select-btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> P15 </button>
+                    <div class="dropdown-menu">
+                        @foreach ($qualities as $qualitylist)
+                            <a class="dropdown-item" href="#">{{ $qualitylist->name }}</a>
+                        @endforeach
+                    </div>
+                </div>
+                <div class="stock-group">
+                    @foreach($selectQuality as $selectQualities)
+                    <div class="stock-container">
+                        <h2>{{ $selectQualities->height }}m</h2>
+                        <h3>{{ $selectQualities->quantity }}</h3>
+                        <p>blocks</p>
+                        <div class="oppervlak">
+                            {{ $selectQualities->height * $selectQualities->quantity }}m³
+                        </div>
+                    </div>
+                    @endforeach
+
+
+                </div>
             </div>
-            <div class="event-log" >
-                <h5>Event log</h5>
+            <div class="frame event" >
+                <div class="frame-title">
+                    <h2>Events loggings</h2> </div>
                 <div class="log-console">
                     @foreach($eventlog as $eventlogs)
                     <p>{{ $eventlogs->action }}</p><p>{{ $eventlogs->sector }}</p>
@@ -46,7 +73,35 @@
             </div>
         </div>
         <div class="right">
+            <div class="frame prime">
+                <div class="frame-title">
+                    <h2>Prime silo status</h2> </div>
+                <div class="silo-group">
+                    @foreach($primesilo as $silos)
+                    <div class="silo-container">
+                        <h3>{{ $silos->id }}</h3>
+                        <div class="silo-graph">
+                            @if( $silos->quantity < 50)
+                                <div class="silo-graph-value" style="height:{{ $silos->quantity * 1.2 }}px; background-color: #5FB760;"> </div>
+                            @elseif($silos->quantity < 90)
+                                <div class="silo-graph-value" style="height:{{ $silos->quantity * 1.2 }}px; background-color: #EEAC57;"> </div>
+                            @else
+                                <div class="silo-graph-value" style="height:{{ $silos->quantity * 1.2 }}px; background-color: #D75452;"> </div>
+                            @endif
+
+                        </div>
+                        <div class="silo-info">
+                            <h3>{{$silos->grondstof->type}}</h3>
+                            <h4>{{ $silos->quantity }}</h4>
+                        </div>
+                    </div>
+                    @endforeach
+
+                </div>
+            </div>
+            <!--
             <div class="cgp">
+
                 <h5>Prime silo</h5>
                 <div class="silo-stats">
                     @foreach($primesilo as $silos)
@@ -66,9 +121,37 @@
 
 
                 </div>
-            </div>
+            </div>-->
             <div class="cgps">
-                <div class="cgr">
+
+                <div class="frame recycle">
+                    <div class="frame-title">
+                        <h2>Recycle silo status</h2> </div>
+                    <div class="silo-group">
+                        @foreach($recyclesilo as $silos)
+                            <div class="silo-container">
+                                <h3>{{ $silos->id }}</h3>
+                                <div class="silo-graph">
+                                    @if( $silos->quantity < 50)
+                                        <div class="silo-graph-value" style="height:{{ $silos->quantity * 1.2 }}px; background-color: #5FB760;"> </div>
+                                    @elseif($silos->quantity < 90)
+                                        <div class="silo-graph-value" style="height:{{ $silos->quantity * 1.2 }}px; background-color: #EEAC57;"> </div>
+                                    @else
+                                        <div class="silo-graph-value" style="height:{{ $silos->quantity * 1.2 }}px; background-color: #D75452;"> </div>
+                                    @endif
+
+                                </div>
+                                <div class="silo-info">
+
+                                    <h4>{{ $silos->quantity }}</h4>
+                                </div>
+                            </div>
+                        @endforeach
+
+                    </div>
+                </div>
+                <!--<div class="cgr">
+
                     <h5>Recycle silo</h5>
                     <div class="silo-stats">
                         @foreach($recyclesilo as $silos)
@@ -86,9 +169,10 @@
                             </div>
                         @endforeach
                     </div>
-                </div>
-                <div class="cgg">
-                    <h5>Resources</h5>
+                </div>-->
+                <div class="frame rawmaterials">
+                    <div class="frame-title">
+                        <h2>Rawmaterials</h2> </div>
                     <div class="char">
                         <div class="pieID pie"> </div>
                     </div>
@@ -107,6 +191,12 @@
     </body>
     <script src="http://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
     <script type="text/javascript">
+
+
+
+
+
+
         function sliceSize(dataNum, dataTotal) {
             return (dataNum / dataTotal) * 360;
         }
