494c23b49d9d29a172c943b520c332877926d75c
wimvermost <vermost.w@gmail.com>
2016-11-12 23:34:50 +0100

    Added new permission table with model, user has permissions tables and the seeders. Added new composer requirement, so don’t forget to composer install

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Seeder;
 use Faker\Factory as Faker;
 use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class UsersTableSeeder extends Seeder
 {
@@ -13,6 +15,25 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+
+        $permission = \App\Permission::create(['name' => 'viewdashboard']);
+        $permission->save();
+        $permission = \App\Permission::create(['name' => 'viewblocks']);
+        $permission->save();
+        $permission = \App\Permission::create(['name' => 'manageblocks']);
+        $permission->save();
+        $permission = \App\Permission::create(['name' => 'viewrecyclesilos']);
+        $permission->save();
+        $permission = \App\Permission::create(['name' => 'managerecyclesilos']);
+        $permission->save();
+        $permission = \App\Permission::create(['name' => 'viewprimesilos']);
+        $permission->save();
+        $permission = \App\Permission::create(['name' => 'manageprimesilos']);
+        $permission->save();
+        $permission = \App\Permission::create(['name' => 'manageusers']);
+        $permission->save();
+
+
         $faker = Faker::create();
         foreach(range(1, 10) as $index)
         {
@@ -21,6 +42,7 @@ class UsersTableSeeder extends Seeder
             $user->email = $faker->email();
             $user->password = Hash::make('admin');
             $user->save();
+            $user->givePermissionTo('viewdashboard');
         }
 
         $user = new \App\User();
@@ -29,5 +51,7 @@ class UsersTableSeeder extends Seeder
         $user->admin = 1;
         $user->password = Hash::make('hisfa');
         $user->save();
+        $user->givePermissionTo('viewdashboard', 'manageblocks', 'viewrecyclesilos', 'managerecyclesilos', 'viewprimesilos', 'manageprimesilos', 'manageusers');
+
     }
 }
