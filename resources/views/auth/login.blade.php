46c405a430fca4db01b6b6fa0e63bc919b257d84
wimvermost <vermost.w@gmail.com>
2016-11-16 15:45:04 +0100

    Added decent layout for password reset page.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,7 +9,7 @@
                         <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
                             {{ csrf_field() }}
 
-                            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+
                                 <label for="email" class="col-md-4 control-label">Email</label>
                                     <input id="email" type="email" placeholder="Email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
 
@@ -18,9 +18,7 @@
                                     </span>
                                     @endif
 
-                            </div>
 
-                            <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
                                 <label for="password" class="col-md-4 control-label">Wachtwoord</label>
                                     <input id="password" placeholder="Wachtwoord"type="password" class="form-control" name="password" required>
 
@@ -30,18 +28,14 @@
                                     </span>
                                     @endif
 
-                            </div>
-
-                            <div class="form-group">
-
-                                    <button type="submit"">
+                                    <button type="submit">
                                         Login
                                     </button>
 
                                     <a class="forgot" href="{{ url('/password/reset') }}">
                                         Wachtwoord vergeten?
                                     </a>
-                            </div>
+
                         </form>
     </div>
 @endsection
