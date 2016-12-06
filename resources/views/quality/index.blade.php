58c93d4e90fb77631180c62d72c189c372a46470
DamonBaesen <r0484656@student.thomasmore.be>
2016-12-06 22:52:09 +0100

    layout all pages

--- resources/views/quality/index.blade.php
+++ resources/views/quality/index.blade.php
@@ -6,19 +6,20 @@
         <link rel="stylesheet" href="/css/block-style.css">
 
     </head>
-    <div class="silo-container">
+    <div class="silo-container" id="qualityContainer">
                 <div class="silo-title"><h1>Quality overview</h1></div>
-                  @foreach ($data as $qualities)
-                       <li>
+        <ul class="qualityList">
+        @foreach ($data as $qualities)
+                       <li class="qualityList">
                         <div class="block-total">
-                            <h1>{{ $qualities->name }}</h1>
-                            <p>Hardness: {{ $qualities->hardness }}</p>
+                            <h1  >{{ $qualities->name }}</h1>
+                            <p class="qualityHeader">Hardness: {{ $qualities->hardness }}</p>
                             <a href="/quality/remove/{{$qualities->id}}" id="delete">Delete</a>
                             <a href="/quality/edit/{{$qualities->id}}" id="edit">Edit</a>
+                            </div>
                        </li>
                 @endforeach
-                <hr/>
-
+        </ul>
             <h5 class="totalAll"><a href="{{ url('/quality/add') }}" id="addQuality">Add quality</a></h5>
         </div>
      </div>
