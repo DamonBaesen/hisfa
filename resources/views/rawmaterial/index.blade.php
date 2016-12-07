2e744dadadeee90c95002c80616de40a1c491264
billie peeters <billie.peeters@hotmail.com>
2016-12-07 09:07:51 +0100

    rawmaterials
    design

--- resources/views/rawmaterial/index.blade.php
+++ resources/views/rawmaterial/index.blade.php
@@ -13,7 +13,15 @@
 
     <div class="raw-container">
         <h1>Rawmaterials</h1>
+            @if(Session::has('success'))
+                            <div class="alert alert-success">
+                            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                            
+                            {{ Session::get('message', '') }}
+                            </div>
+                            @endif
             <div class="raw-overview">
+                  
                    @foreach ($rawmaterial as $rawmaterials)
                     @if($rawmaterials->stock != 0)
                       <li>
@@ -37,9 +45,15 @@
                                 Delivered: {{$rawmaterials->deliverd}} oct.
                            </p>
                            <div class="line"></div>
+                           @if($rawmaterials->using == 0)
                            <div class="checkbox">
+                               <input type="checkbox" name="using"  disabled> <p>Using</p>
+                           </div>
+                           @else
+                            <div class="checkbox">
                                <input type="checkbox" name="using" checked disabled> <p>Using</p>
                            </div>
+                           @endif
                             
                             <a href="/rawmaterial/edit/{{$rawmaterials->id}}" id="editRawmaterial">Edit</a>
                             
@@ -50,8 +64,12 @@
                             
                          </div>
                          </li>
+                         
                     
                      </div>
+                     
+                    
+                     
                      <div class="addTotal">
                               <a href="/rawmaterial/add"> +</a>
                           </div> 
