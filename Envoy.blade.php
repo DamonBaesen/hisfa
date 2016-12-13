c9e9624eb2f24fd53caede06f0c67ea376a20f07
wimvermost <vermost.w@gmail.com>
2016-12-13 22:42:03 +0100

    Changes to envoy file

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,11 +1,12 @@
-@servers([‘production’ => [deploybot@ 139.162.191.104]])
-@task(deploy, ['on' => production])
-cd /home/hisfa/hisfa
-php artisan down
-git reset --hard HEAD
-git pull origin master
-composer dump-autoload
-php artisan migrate:refresh --seed
-php artisan up
+@servers(['web' => 'deploybot@139.162.191.104'])
+
+@macro('deploy', ['on' => 'web'])
+	run
+@endmacro
+
+@task('run')
+	cd /home/hisfa/hisfa
+	php artisan down
 @endtask
+
 
